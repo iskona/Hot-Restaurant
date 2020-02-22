a67fd54fc0e9dac332fb84134fd247a826e4d347
iskona <isony.d@gmail.com>
2020-02-22 14:05:46 -0800

    changed logic on clear

--- server.js
+++ server.js
@@ -15,71 +15,68 @@ app.use(express.json());
 //Reservation array
 
 var reserved = [];
-const waitList = [];
+
+var waitList = [];
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-
-// app.get("/", function(req, res){
-//     res.send("HomePage");
-// });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
 
 //View Tables
 
-app.get("/table", function(req, res){
-    res.sendFile(path.join(__dirname, "table.html"))
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "table.html"))
 })
 
-app.get("/api/table", function(req, res){
-    return res.json(reserved);
+app.get("/api/table", function (req, res) {
+  return res.json(reserved);
 });
 
 //Send waitlist json (APIWaitlistLink)
 
-app.get("/api/waitlist", function(req, res){
-    return res.json(waitList);
+app.get("/api/waitlist", function (req, res) {
+  return res.json(waitList);
 });
 
 //
 
-app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, "reserve.html"))
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"))
 })
-
-// app.get("/reserve", function(req, res){
-//     res.send("Reservation Page");
-// })
 
 //Post
 
-app.post("/reserve", function(req, res){
-    var customerData = req.body;
+app.post("/reserve", function (req, res) {
+  var customerData = req.body;
 
-    customerData.routeName = customerData.name.replace(/\s+/g, "").toLowerCase();
+  customerData.routeName = customerData.name.replace(/\s+/g, "").toLowerCase();
 
-    if(reserved.length <= 4)
-    {
-        reserved.push(customerData);
-        res.send("reserved");
-    }
-    else{
+  if (reserved.length <= 4) {
+    reserved.push(customerData);
+    res.send("reserved");
+  }
+  else {
 
-        waitList.push(customerData);
-        res.send("waitlist");
-    }
+    waitList.push(customerData);
+    res.send("waitlist");
+  }
 
-    console.log(reserved)
+  console.log(reserved)
 })
 
-app.get("/clear", function(req, res){
-    reserved = [];
-    res.sendFile(path.join(__dirname, "table.html"));
-})
+app.get("/clear", function (req, res) {
+  reserved = [];
+  let len = (waitList.length > 5) ? 5 : waitList.length;
+  for (let i = 0; i < len; i++) {
+    reserved.push(waitList[i]);
+    waitList.shift();
+  }
+  res.sendFile(path.join(__dirname, "table.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App lostening on port " + PORT);
+app.listen(PORT, function () {
+  console.log("App lostening on port " + PORT);
 });
